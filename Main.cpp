2dc5cb970b2638afa46e0383c8b3ef2d97abfa9a
Леонид Власов <khromtsov03@mail.ru>
2022-08-15 17:37:16 +0300

    Реализован конструктор с параметрами, который принимает строку и копирует её значения в строку, объявленную в классе MyString.

--- Main.cpp
+++ Main.cpp
@@ -11,8 +11,15 @@ public:
 
 	MyString(char *pStr)
 	{
-		int iLength = strlen(pStr);
-		this->pStr = pStr;
+		int iLength = strlen(pStr); // —читываем длину строки.
+		this->pStr = new char[iLength + 1]; // ¬ыдел€ем динамическую пам€ть дл€ указател€ pStr с размером iLength + 1 (дл€ добавлени€ в конец строки нуль-терминатора).
+
+		for (int i = 0; i < iLength; ++i)
+		{
+			this->pStr[i] = pStr[i]; //  аждый элемент полученного массива char'ов копируетс€ в строку pStr, объ€вленную в классе.
+		}
+
+		this->pStr[iLength] = '\0'; // ¬ конец строки добавл€ем нуль-терминатор.
 	}
 
 	~MyString()
@@ -20,7 +27,7 @@ public:
 
 	}
 private:
-	char *pStr;
+	char *pStr; // —оздаем указатель на char дл€ прин€ти€ строки с произвольной длиной.
 };
 
 
