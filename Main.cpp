95e05c15d7b7afc93b508367f65e7a6900761aa6
Леонид Власов <khromtsov03@mail.ru>
2022-08-15 17:23:08 +0300

    Добавление указателя на char, конструктора по умолчанию, конструктора с параметрами (принимает строку) и деструктора.

--- Main.cpp
+++ Main.cpp
@@ -4,9 +4,23 @@ using namespace std;
 class MyString
 {
 public:
+	MyString()
+	{
+		pStr = nullptr;
+	}
 
+	MyString(char *pStr)
+	{
+		int iLength = strlen(pStr);
+		this->pStr = pStr;
+	}
+
+	~MyString()
+	{
+
+	}
 private:
-
+	char *pStr;
 };
 
 
